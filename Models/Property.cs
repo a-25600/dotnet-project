05ac485edcfd5846264b481216a79b1eda6149cc
a-25600 <aleksagunya56@gmail.com>
2026-09-15 22:17:12 +0300

    Add Property

---
+++ Models/Property.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstateApi.Models;
+
+public class Property
+{
+    public int Id { get; set; }
+    public string Address { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public string Type { get; set; } = string.Empty;
+    public int RoomsCount { get; set; }
+    public bool IsAvailable { get; set; } = true;
+}
