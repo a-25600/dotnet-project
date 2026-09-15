385d7d301a2134cea88c505bfeeb55456137b67d
a-25600 <aleksagunya56@gmail.com>
2026-09-15 22:16:34 +0300

    PropertyDTOs.cs

---
+++ DTOs/PropertyDTOs.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RealEstateApi.DTOs;
+
+public class PropertyDto
+{
+    [Required(ErrorMessage = "Address is requied")]
+    [StringLength(200, MinimumLength = 5)]
+    public string Address { get; set; } = string.Empty;
+
+    [Range(100, 10000000, ErrorMessage = "Invalid price")]
+    public decimal Price { get; set; }
+
+    public string Type { get; set; } = string.Empty;
+
+    [Range(1, 20, ErrorMessage = "Rooms number must be from 1 to 20")]
+    public int RoomsCount { get; set; }
+
+    public bool IsAvailable { get; set; } = true;
+}
+
+public class PropertyQueryParameters
+{
+    public decimal? MaxPrice { get; set; }
+    public int? MinRooms { get; set; }
+    public bool? IsAvailable { get; set; }
+}
